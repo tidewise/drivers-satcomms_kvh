778c9f6737410a79d55aa6f1abb169cd371cca60
Afonso da Fonseca Braga <afonso.braga@engenharia.ufjf.br>
2022-12-12 15:19:59 -0300

    Initial commit

---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <satcomms_kvh/Dummy.hpp>
+
+int main(int argc, char** argv)
+{
+    satcomms_kvh::DummyClass dummyClass;
+    dummyClass.welcome();
+
+    return 0;
+}
